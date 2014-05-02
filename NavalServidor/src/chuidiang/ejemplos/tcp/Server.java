93b4e0eb0f08385ecb93ba8980d282770d60a24a
oscar <oscarmesa.elpoli@gmail.com>
2014-05-02 15:32:10 -0500

    cambios para diego

--- NavalServidor/src/chuidiang/ejemplos/tcp/Server.java
+++ NavalServidor/src/chuidiang/ejemplos/tcp/Server.java
@@ -12,22 +12,26 @@ package chuidiang.ejemplos.tcp;
 import java.io.*;
 
 import java.net.ServerSocket;
-
 import java.net.Socket;
-
 import java.net.SocketException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import navalcliente.Jugador;
+import navalcliente.Mensaje;
 
 public class Server {
 
     private ServerSocket serverSocket = null;
     private Socket socket = null;
     private ObjectInputStream inStream = null;
-    
-    private HashMap<String,Jugador> jugadores;
-    
+    private boolean asignado = false;
+
+    private HashMap<String, Jugador> jugadores;
+    private ObjectOutputStream outputStream;
+
     public Server() {
         jugadores = new HashMap<>();
     }
@@ -36,41 +40,34 @@ public class Server {
         try {
             serverSocket = new ServerSocket(4445);
             System.out.println("Connected");
-             
-             while (true) {
+
+            while (true) {
                 socket = serverSocket.accept();
-                 
-                inStream = new ObjectInputStream(socket.getInputStream()); 
-                 
+                inStream = new ObjectInputStream(socket.getInputStream());
                 Jugador j = (Jugador) inStream.readObject();
-                
                 Jugador p = jugadores.get(j.getIdjugador().toString());
-                if(p == null)
-                {
+                if (p == null) {
                     System.out.println("No existe, agregado");
                     jugadores.put(j.getIdjugador().toString(), j);
-                    if(jugadores.size() == 2)
-                    {
-                        asignarOponentes();
+                    if (jugadores.size() == 2 && !asignado) {
+                        asignado = true;
+                        asignarOponentes(j.getIdjugador());
+                    }else{
+                        jugadores.get(j.getIdjugador().toString()).setMsn(Mensaje.ESPERA_OPONENTE);
                     }
-                }else{
-                    System.out.println("ya existe");
+                } else {
+                   System.out.println("ya existe");
                 }
                 System.out.println(j);
+                outputStream = new ObjectOutputStream(socket.getOutputStream());
+                outputStream.writeObject(jugadores.get(j.getIdjugador().toString()));
             }
-
         } catch (SocketException se) {
-
             System.exit(0);
-
         } catch (IOException e) {
-
             e.printStackTrace();
-
         } catch (ClassNotFoundException cn) {
-
             cn.printStackTrace();
-
         }
         try {
             socket.close();
@@ -80,15 +77,23 @@ public class Server {
     }
 
     public static void main(String[] args) {
-
         Server server = new Server();
-
         server.communicate();
-
     }
 
-    private void asignarOponentes() {
-      
+    private void asignarOponentes(String id) {
+        List keys = new ArrayList(jugadores.keySet());
+        Jugador j1, j2;
+        jugadores.get(keys.get(0)).setOpenente(jugadores.get(keys.get(1)));
+        jugadores.get(keys.get(1)).setOpenente(jugadores.get(keys.get(0)));
+        
+        jugadores.get(id).setMsn(navalcliente.Mensaje.TURNO);
+        
+        if(!jugadores.get(keys.get(0)).getIdjugador().equals(id) ){
+            jugadores.get(keys.get(0)).setMsn(Mensaje.ESPERA_MOVIMIENTO_OPONENTE);
+        }else{
+            jugadores.get(keys.get(1)).setMsn(Mensaje.ESPERA_MOVIMIENTO_OPONENTE);
+        }
     }
 
 }
