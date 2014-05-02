e63a844694891052660d2d6388f5b28e253e3c41
Dehost <diego8525@gmail.com>
2014-05-02 18:22:35 -0500

    cambios

---
+++ memoria/memoria_cliente/src/memoria_cliente/Memoria_cliente.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package memoria_cliente;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Memoria_cliente {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
---
+++ memoria/memoria_servidor/src/memoria_servidor/Memoria_servidor.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package memoria_servidor;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Memoria_servidor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
